2005761a58ffecae3bd372c99111b6d99306e43b
ismaail <contact@ismaail.com>
2024-09-08 19:18:29 +0100

    Add missing wire:key to $column foreach

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,6 +1,6 @@
 <div class="flex grow-0 space-x-4">
 	@foreach($columns as $column)
-		<div class="flex flex-col border w-1/3 p-2 space-y-0 rounded-2xl">
+		<div wire:key="column-{{ $column->id }}" class="flex flex-col border w-1/3 p-2 space-y-0 rounded-2xl">
 			<div class="p-2 font-bold bg-gray-100 rounded-t-2xl">{{ $column->name }}</div>
 			<livewire:column-component :key="$column->id" :column="$column" />
 		</div>
